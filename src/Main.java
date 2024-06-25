f455e2d3b3390331fe87fea2cb297e2463577f2c
vigRudolf <vrudi68@gmail.com>
2024-06-25 18:22:17 +0200

    the program is too slow, i need to get the starters individual cycles

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -41,7 +43,52 @@ public class Main {
                 counterForSteps = 0;
             }
         }
-
         System.out.println("Result is: " + counterForResults + " steps");
+
+        /*Pattern patternForEndWithA = Pattern.compile("^.{2}A$");
+        Pattern patternForEndWithZ = Pattern.compile("^.{2}Z$");*/
+
+        ArrayList<String> starters = new ArrayList<>();
+        for(String key : map.keySet()){
+            if(key.charAt(2) == 'A') {
+                starters.add(key);
+            }
+        }
+
+        int counterForResultsPart2 = 0;
+        int counterForStepsPart2 = 0;
+        boolean IsNotFound = true;
+
+        while(IsNotFound) {
+
+            int finalCounterForSteps = counterForStepsPart2;
+            starters.replaceAll(key -> map.get(key)[leftOrRight[finalCounterForSteps]]);
+
+            counterForResultsPart2++;
+
+            if (counterForStepsPart2 == leftOrRight.length - 1) {
+                counterForStepsPart2 = 0;
+            } else {
+                counterForStepsPart2++;
+            }
+
+            boolean allIsEndWithZ = true;
+            for(String str : starters) {
+                if(str.charAt(2) != 'Z') {
+                    allIsEndWithZ = false;
+                    break;
+                }
+            }
+            if(allIsEndWithZ) {
+                IsNotFound = false;
+            }
+            if(counterForResultsPart2 % 100000 == 0){
+                System.out.println(counterForResultsPart2);
+            }
+
+        }
+
+        System.out.println("Result of part2: " + counterForResultsPart2);
+
     }
 }
